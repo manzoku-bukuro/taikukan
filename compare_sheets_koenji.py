d8cdbc392ffae6bc17835b964578f82658c0c165
manzoku-bukuro <manzoku.bukuro@gmail.com>
2024-10-24 13:42:19 +0900

    .text

--- compare_sheets_koenji.py
+++ compare_sheets_koenji.py
@@ -129,7 +129,7 @@ def main():
             print('selector:',selector)
             driver.execute_script(selector)
             # HEADINFOとういclassがあるかどうかを確認
-            head_info = driver.find_elements(By.CLASS_NAME, "HEADINFO")
+            head_info = driver.find_elements(By.CLASS_NAME, "HEADINFO").text
             print('head_info:',head_info)
 
     
